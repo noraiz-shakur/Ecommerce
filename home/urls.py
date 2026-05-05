b5d89f69fdbe55fd7b0e6a4368fe38019a0fd1ec
Noraiz Shakur <noraizahmad128@gmail.com>
2026-05-05 20:51:42 +0500

    crud operations

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.restaurant_list, name="restaurant_list"),
     path("restaurant/<int:id>/", views.restaurant_detail, name="restaurant_detail"),
+    path("add/", views.add_menu, name="add_menu"),
+    path("edit/<int:id>/", views.edit_menu, name="edit_menu"),
+    path("delete/<int:id>/", views.delete_menu, name="delete_menu"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Restaurant, Menu
 
 
@@ -19,3 +19,44 @@ def restaurant_detail(request, id):
     return render(
         request, "home/details.html", {"restaurant": restaurant, "menus": menus}
     )
+
+
+def add_menu(request):
+    restaurants = Restaurant.objects.all()
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        restaurant_id = request.POST.get("restaurant")
+
+        restaurant = Restaurant.objects.get(id=restaurant_id)
+
+        Menu.objects.create(restaurant=restaurant, name=name, price=price)
+
+        return redirect("restaurant_list")
+
+    return render(request, "home/add_menu.html", {"restaurants": restaurants})
+
+
+def read_menu(request):
+    menus = Menu.objects.all()
+
+
+def edit_menu(request, id):
+    menu = get_object_or_404(Menu, id=id)
+
+    if request.method == "POST":
+        menu.name = request.POST["name"]
+        menu.price = request.POST["price"]
+        menu.save()
+
+        return redirect("restaurant_list")
+
+    return render(request, "home/edit_menu.html", {"menu": menu})
+
+
+def delete_menu(request, id):
+    menu = get_object_or_404(Menu, id=id)
+    menu.delete()
+
+    return redirect("restaurant_list")
